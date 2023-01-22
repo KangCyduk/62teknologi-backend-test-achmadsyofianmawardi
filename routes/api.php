256d908695156dda4a0f500d21f8811c013d2ec7
Achmad Mawardi <asmaward@pmintl.net>
2023-01-22 10:50:37 +0700

    fix bug in route

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-https: //api.yelp.com/v3/businesses/search?location=us&sort_by=best_match&limit=20
+//https: //api.yelp.com/v3/businesses/search?location=us&sort_by=best_match&limit=20
 Route::post('/business', [businessesController::class, 'store']);
 Route::put('/business', [businessesController::class, 'update']);
 Route::delete('/business', [businessesController::class, 'delete']);
